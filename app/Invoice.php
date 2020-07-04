2f9fec8928c76dac6083ada9479ca06436a65601
Anistya Dwi Setiawan <anistyadwisetiawan04@gmail.com>
2020-07-04 11:58:45 +0700

    Memperbaiki eror pencarian invoice pada file model invoice

--- app/Invoice.php
+++ app/Invoice.php
@@ -17,10 +17,7 @@ class Invoice extends Model
     public function scopeFilter($query, array $filters)
     {
         $query->when($filters['search'] ?? null, function ($query, $search) {
-            $query->whereHas('room', function ($query) use ($search) {
-                $query->where('number', 'like', "$search%");
-            });
-            $query->orWhereHas('renter', function ($query) use ($search) {
+            $query->whereHas('bill', function ($query) use ($search) {
                 $query->where('name', 'like', "$search%");
             });
         })->when($filters['trashed'] ?? null, function ($query, $trashed) {
